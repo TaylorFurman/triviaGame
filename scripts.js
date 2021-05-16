990c2c82ab2856103164c2801ccc7104e2acdeff
unknown <TFurman47@gmail.com>
2021-05-16 10:52:27 -0500

    updated and now have a working product

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
-let gameCounter = document.getElementById("gameCounter");
-
 //Score variables
 let player1Score = document.getElementById("p1Score")
 let player2Score = document.getElementById("p2Score")
 
 
-let playerTurn = document.getElementById("playerTurn")
+
+//determining who's turn it is based on the window refresh counter
+let pTurn = document.getElementById("playerTurn")
+
+function playerTurn(){
+  if(localStorage.getItem("counter")%2 !=0){
+    pTurn.innerHTML = "Player 1's Turn!";
+  }else if (localStorage.getItem("counter")%2 ==0){
+    pTurn.innerHTML = "Player 2's Turn!";
+  }
+}
+playerTurn();
+
 
 function spawnQuestion(){
   axios
@@ -34,36 +44,41 @@ function spawnQuestion(){
 
 //When buttons are clicked, do the following
 $('#answer').click(() =>{
-  gameCounter++;
-  checkScore();
-  scoreUpdate();
-  attemptUpdate();
-  gameCounterClock();
-  console.log(gameCounter);
+  addPoint();
 })
 
 $('#wrongChoice1').click(() =>{
-  gameCounter++;
   alert("wrong answer");
-  console.log(gameCounter);
+  takePoint();
+  window.location.reload();
 })
 
 $('#wrongChoice2').click(() =>{
-  gameCounter++;
   alert("wrong answer");
-  console.log(gameCounter);
+  takePoint();
+  window.location.reload();
 })
 
 $('#wrongChoice3').click(() =>{
-  gameCounter++;
   alert("wrong answer");
-  console.log(gameCounter);
+  takePoint();
+  window.location.reload();
+})
+
+$('#newGame').click(() =>{
+  alert("NEW GAME STARTED!")
+  localStorage.removeItem('p1Score');
+  localStorage.removeItem('p2Score');
+  localStorage.removeItem('counter');
+  window.location.reload();
 })
 
 //Calls API and displays question/answer choices
 
 spawnQuestion()
 
+//checks score and clears local storage when players win
+checkScore()
 
 //Checking the local browser storage to see if "Score" exists.  If not it is created and set to 0.
 if(localStorage.getItem("p1Score")){
@@ -85,68 +100,63 @@ else{
   p2Score = 0;
 }
 
-if(localStorage.getItem("gameCounter")){
-  gameCounter.innerHTML = localStorage.getItem("gameCounter");
-  gameCounter = localStorage.getItem("gameCounter");
-}
-else{
-  gameCounter.innerHTML = 0;
-  gameCounter = 0;
-}
-
+//functions for checking score to check for win condition
 function checkScore(){
-  if(p1Score == 5){
+  if(localStorage.getItem('p1Score') == 10){
     alert("Player 1 wins!!")
-    localStorage.clear();
-    window.location.reload();
+    localStorage.removeItem('p1Score');
+    localStorage.removeItem('p2Score');
+    localStorage.removeItem('counter');
+    window.location.reload();//need to clear the window reload count in local storage
+  }
+  else if (localStorage.getItem('p2Score') == 10){
+    alert("Player 2 wins!!")
+    localStorage.removeItem('p1Score');
+    localStorage.removeItem('p2Score');
+    localStorage.removeItem('counter');
+    window.location.reload();//need to clear the window reload count in local storage
   }
 }
-
-function scoreUpdate(){
-  p1Score = parseInt(p1Score) + 1;
+//functions for adding a point based on correct choice
+function addPoint(){
+  if(localStorage.getItem('counter')%2 != 0){
+    p1Score = parseInt(p1Score) + 1;
     localStorage.setItem("p1Score", p1Score);
     alert("Player 1's score is: " + p1Score);
     location.reload();
+  }
+  else if (localStorage.getItem('counter')%2 == 0){
+    p2Score = parseInt(p2Score) + 1;
+    localStorage.setItem("p2Score", p2Score);
+    alert("Player 2's score is: " + p2Score);
+    location.reload();
+  }
+}
+//functions for taking a point based on correct choice
+function takePoint(){
+  if(localStorage.getItem('counter')%2 != 0 && localStorage.getItem('p1Score') >0){
+    p1Score = parseInt(p1Score) - 1;
+    localStorage.setItem("p1Score", p1Score);
+    alert("Player 1's score is: " + p1Score);
+    location.reload();
+  }
+  else if (localStorage.getItem('counter')%2 == 0 && localStorage.getItem('p2Score') >0){
+    p2Score = parseInt(p2Score) - 1;
+    localStorage.setItem("p2Score", p2Score);
+    alert("Player 2's score is: " + p2Score);
+    location.reload();
+  }
 }
 
-function gameCounterClock(){
-  gameCounter = parseInt(gameCounter);
-  localStorage.setItem("gameCounter", gameCounter);
-  location.reload();
+console.log(localStorage.getItem('counter'));
+
+//keep track of how many times the window has been refreshed to determine player's turn/score update
+function windReloadCounter(){
+  window.addEventListener("unload", function(){
+    var count = parseInt(localStorage.getItem('counter') || 0);
+    localStorage.setItem('counter', ++count);
+  }, false); 
 }
 
-function attemptUpdate(){
-  p1trial = parseInt(localStorage.getItem('p1Attempts'))
-  p1Attempts = parseInt(p1Attempts) + p1trial;
-  localStorage.setItem("p1Attempts", p1Attempts);
-  alert("Player 1's total attempts are: " + p1Attempts);
-}
-
-//Below needs work
-
-let player1Attempts = document.getElementById("p1Attempts")
-let player2Attempts = document.getElementById("p2Attempts")
-
-
-
-
-if(localStorage.getItem("p1Attempts")){
-  player1Attempts.innerHTML = localStorage.getItem("p1Attempts");
-  p1Attempts = localStorage.getItem("p1Attempts");
-}
-else{
-  player1Attempts.innerHTML = 0;
-  p1Attempts = 0;
-}
-
-
-if(localStorage.getItem("p2Attempts")){
-  player1Attempts.innerHTML = localStorage.getItem("p2Attempts");
-  p2Attempts = localStorage.getItem("p2Attempts");
-
-}
-else{
-  player1Attempts.innerHTML = 0;
-  p2Attempts = 0;
-}
+windReloadCounter()
 
--- scripts3.js
+++ scripts3.js
@@ -49,3 +49,68 @@ axios
     })      
     
     .catch("failure");
+
+
+
+//Below needs work
+
+let player1Attempts = document.getElementById("p1Attempts")
+let player2Attempts = document.getElementById("p2Attempts")
+
+function attemptUpdate(){
+    p1Attempts = parseInt(p1Attempts);
+    localStorage.setItem("p1Attempts", p1Attempts);
+    alert("Player 1's total attempts are: " + p1Attempts);
+  }
+
+
+
+
+if(localStorage.getItem("p1Attempts")){
+  player1Attempts.innerHTML = localStorage.getItem("p1Attempts");
+  p1Attempts = localStorage.getItem("p1Attempts");
+}
+else{
+  player1Attempts.innerHTML = 0;
+  p1Attempts = 0;
+}
+
+
+if(localStorage.getItem("p2Attempts")){
+  player1Attempts.innerHTML = localStorage.getItem("p2Attempts");
+  p2Attempts = localStorage.getItem("p2Attempts");
+
+}
+else{
+  player1Attempts.innerHTML = 0;
+  p2Attempts = 0;
+}
+
+
+
+function gameCounterClock(){
+    console.log(gameCounter);
+    gameCounter = parseInt(gameCounter);
+    localStorage.setItem("gameCounter", gameCounter);
+    localStorage.removeItem("gameCounter");
+    location.reload();
+  }
+
+if(localStorage.getItem("gameCounter")){
+gameCounter.innerHTML = localStorage.getItem("gameCounter");
+gameCounter = localStorage.getItem("gameCounter");
+}
+else{
+gameCounter.innerHTML = 0;
+gameCounter = 0;
+}
+
+
+if(localStorage.getItem("windowReloadCounter")){
+windowReloadCounter.innerHTML = localStorage.getItem("windowReloadCounter");
+windowReloadCounter = localStorage.getItem("windowReloadCounter");
+}
+else{
+windowReloadCounter.innerHTML = 0;
+windowReloadCounter = 0;
+}
